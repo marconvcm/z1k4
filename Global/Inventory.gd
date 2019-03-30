dd07b40835cb98d0475100021aa155d4827837a2
Marcos Vinícius Marcon <marconvcm@gmail.com>
2019-03-30 16:35:33 -0300

    Changed gles

---
+++ Global/Inventory.gd
@@ -0,0 +1,14 @@
+extends Node
+
+var data: Array
+
+func _ready():
+	awake()
+
+func awake():
+	var file = File.new()
+	file.open("res://Inventory.json", File.READ)
+	var json = file.get_as_text()
+	data = JSON.parse(json).result
+	file.close()
+	pass
---
+++ Global/Item.gd
@@ -0,0 +1,7 @@
+extends Node
+
+var id: int
+var label: String
+var information: String
+var icon: String
+var amount: int
